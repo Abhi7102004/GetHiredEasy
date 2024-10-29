9cb377da931c1644655f9a03f1df5a9a08a376dd
Abhi7102004 <abhishekyadav7102004@gmail.com>
2024-10-29 15:31:34 +0530

    added all origins

--- server/utils/db.js
+++ server/utils/db.js
@@ -1,10 +1,19 @@
-import mongoose from "mongoose";
+const mongoose = require("mongoose");
+
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGODB_URL);
-    console.log('mongoDb connected')
+    const opts = {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      serverSelectionTimeoutMS: 5000, // Timeout after 5s instead of 30s
+      socketTimeoutMS: 45000, // Close sockets after 45s of inactivity
+    };
+
+    await mongoose.connect(process.env.MONGODB_URI, opts);
+    console.log("MongoDB Connected...");
   } catch (err) {
-    console.log("Database Connection Failed", err.message);
+    console.error("MongoDB connection error:", err);
+    process.exit(1);
   }
 };
-export default connectDB
+export default connectDB;
