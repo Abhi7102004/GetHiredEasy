fe7da885944b66a4c591ee1114961b2aa53d116a
Abhi7102004 <abhishekyadav7102004@gmail.com>
2024-10-29 15:48:34 +0530

    added all origins

--- server/app.js
+++ server/app.js
@@ -18,7 +18,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
 const corsOption = {
-  origin: "https://get-hired-easy.vercel.app",
+  origin: "http://localhost:5173",
   credentials: true,
   methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
 };
