fbb8983a6d731a3a1b94c8c0d9aac2e0d740f292
Abhi7102004 <abhishekyadav7102004@gmail.com>
2024-10-29 20:00:52 +0530

    changed mongodb url

--- server/app.js
+++ server/app.js
@@ -18,7 +18,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
 const corsOption = {
-  origin: "http://localhost:5173",
+  origin: "https://get-hired-easy-actx.vercel.app/",
   credentials: true,
   methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
 };
