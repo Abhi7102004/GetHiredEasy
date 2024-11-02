46f98b3388a64895c0dc8500130690386c832687
Abhi7102004 <abhishekyadav7102004@gmail.com>
2024-11-03 00:08:16 +0530

    Preparing for deployment

--- server/app.js
+++ server/app.js
@@ -7,12 +7,12 @@ import userRoute from "./routes/user-routes.js";
 import companyRoute from "./routes/company-routes.js";
 import jobRoute from "./routes/job-routes.js";
 import applicationRoute from "./routes/application-route.js";
-import reportIssueRoute from "./routes/report-issue-route.js"
+import reportIssueRoute from "./routes/report-issue-route.js";
 const app = express();
-// import path from "path";
+import path from "path";
 dotenv.config();
 const PORT = process.env.PORT || 3000;
-// const _dirname=path.resolve();
+const _dirname = path.resolve();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
@@ -24,19 +24,22 @@ const corsOption = {
 };
 
 app.use(cors(corsOption));
-app.get("/",(req,res)=>{
-  res.send("Hello from backend")
-})
+app.get("/", (req, res) => {
+  res.send("Hello from backend");
+});
 app.use("/api/v1/user", userRoute);
 app.use("/api/v1/company", companyRoute);
 app.use("/api/v1/job", jobRoute);
 app.use("/api/v1/application", applicationRoute);
-app.use("/api/v1/report/issue",reportIssueRoute);
+app.use("/api/v1/report/issue", reportIssueRoute);
+
+app.use(express.static(path.join(_dirname, "/client/dist")));
+app.get("*", (_, res) => {
+  res.sendFile(path.resolve(_dirname, "client", "dist", "index.html"));
+});
+
 app.listen(PORT, () => {
   connectDB();
   console.log(`Server is running on port ${PORT}`);
 });
 
-// "dev": "nodemon server/app.js"
-// "build": "npm install && npm install --prefix client && npm run build --prefix client"
-// "start": "nodemon server/app.js"
