50f6b5926fabffc69a91d5fd76709db514679702
Abhi7102004 <abhishekyadav7102004@gmail.com>
2024-11-04 00:38:36 +0530

    Render final

--- server/app.js
+++ server/app.js
@@ -18,7 +18,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
 const corsOption = {
-  origin: "https://celebrated-pasca-601035.netlify.app",
+  origin: "https://gethiredeasy-v1.onrender.com",
   credentials: true,
   methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
 };
